c429ec04f0401a2c50c102ffc6532379203e9a4b
洪远洋 <hongyuanyang@huobi.com>
2019-01-28 20:15:45 +0800

    compress public key to address

--- main.go
+++ main.go
@@ -8,8 +8,9 @@ import (
 )
 
 func main() {
-	address, err := primitives.BitCoinHashToAddress("9d3e745da6c1e85960f8a72fc57d0e9c41287d39", primitives.P2SH)
+	//address, err := primitives.BitCoinHashToAddress("9d3e745da6c1e85960f8a72fc57d0e9c41287d39", primitives.P2SH)
 	//address, err := primitives.BitCoinHashToAddress("78c0c1bd8bf13af60f4b0371c2f5f9353de777c9", primitives.P2PKH)
+	address, err := primitives.BitCoinHashToAddress("03635e5c184951e14fcfecc83b15960594f4fceec729e09a4a517b0a03a7f4b9", primitives.P2PKH)
 	if err != nil {
 		panic(err)
 	}
